c4f7d17f7f205964f32a454601bc769a4e9d0910
Sergio Fernandez <sergiorodrigofernandeztesta@gmail.com>
2021-12-17 01:51:55 -0400

    first gui

--- exported/main.py
+++ exported/main.py
@@ -9,17 +9,11 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
-    def __init__(self, MainWindow):
+    def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(640, 480)
-        self.main_window = MainWindow
-        self.setupUi()
-    
-    def setupUi(self):
-        self.centralwidget = QtWidgets.QWidget(self.main_window)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        
-        #region buttons
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton.setGeometry(QtCore.QRect(420, 230, 89, 25))
         self.pushButton.setObjectName("pushButton")
@@ -38,12 +32,6 @@ class Ui_MainWindow(object):
         self.pushButton_6 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_6.setGeometry(QtCore.QRect(540, 330, 89, 25))
         self.pushButton_6.setObjectName("pushButton_6")
-        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_7.setGeometry(QtCore.QRect(530, 10, 89, 25))
-        self.pushButton_7.setObjectName("pushButton_7")
-        #endregion buttons
-        
-        #region labels
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(480, 190, 81, 17))
         self.label.setObjectName("label")
@@ -53,46 +41,49 @@ class Ui_MainWindow(object):
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(20, 20, 161, 17))
         self.label_3.setObjectName("label_3")
+        self.pushButton_7 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_7.setGeometry(QtCore.QRect(530, 10, 89, 25))
+        self.pushButton_7.setObjectName("pushButton_7")
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(0, 435, 640, 25))
+        self.label_4.setGeometry(QtCore.QRect(0, 416, 640, 21))
         self.label_4.setStyleSheet("background-color: black;\n"
-        "color: white;\n"
-        "padding: 5px 0;")
+"color: white;\n"
+"padding: 5px 0;")
         self.label_4.setObjectName("label_4")
-        #endregion labels
-        
-        self.main_window.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(self.main_window)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 22))
         self.menubar.setObjectName("menubar")
-        self.main_window.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(self.main_window)
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
-        self.main_window.setStatusBar(self.statusbar)
+        MainWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi()
-        QtCore.QMetaObject.connectSlotsByName(self.main_window)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self):
-        self.main_window.setWindowTitle("MainWindow")
-        self.pushButton.setText("Original")
-        self.pushButton_2.setText("Median")
-        self.pushButton_3.setText("Canny edge")
-        self.pushButton_4.setText("Blur")
-        self.pushButton_5.setText("Gaussian")
-        self.pushButton_6.setText("Grayscale")
-        self.label.setText("Filters")
-        self.label_2.setText("Path")
-        self.label_3.setText("Proyecto Final IMT-231")
-        self.pushButton_7.setText("X")
-        self.label_4.setText("Gabriela Zubieta")
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Original"))
+        self.pushButton_2.setText(_translate("MainWindow", "Median"))
+        self.pushButton_3.setText(_translate("MainWindow", "Canny edge"))
+        self.pushButton_4.setText(_translate("MainWindow", "Blur"))
+        self.pushButton_5.setText(_translate("MainWindow", "Gaussian"))
+        self.pushButton_6.setText(_translate("MainWindow", "Grayscale"))
+        self.label.setText(_translate("MainWindow", "Filters"))
+        self.label_2.setText(_translate("MainWindow", "Path"))
+        self.label_3.setText(_translate("MainWindow", "Proyecto Final IMT-231"))
+        self.pushButton_7.setText(_translate("MainWindow", "X"))
+        self.label_4.setText(_translate("MainWindow", "Gabriela Zubieta"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow(MainWindow)
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
 
